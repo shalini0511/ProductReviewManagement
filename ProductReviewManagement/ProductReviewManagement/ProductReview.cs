dbcd0a2c389dfe94c81bab4f5f555abf7162ba28
shalini <shalini.v@tvsnext.io>
2021-08-05 16:10:41 +0530

    [shalini]initial commit

---
+++ ProductReviewManagement/ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    //getter and setter fieldes
+    public class ProductReview
+    {
+
+        public int productId { get; set; }
+        public int userId { get; set; }
+        public int rating { get; set; }
+        public string review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ProductReviewManagement;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prodcut Review Management!!!!!!!!");
+            Console.WriteLine("Adding a Prodcut Review In list");
+            Console.WriteLine("Enter Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Creating a list for Product Review
+            List<ProductReview> productReviews = new List<ProductReview>();
+            switch (option)
+            {
+                case 1:
+                    ProductReviewManager.AddingProductReview(productReviews);
+                    break;
+            }
+        }
+    }
+}
